d73624146093451ca30157507187b47c2d720d99
Ralph Olazo <ralpholazo@spectres.solutions>
2018-06-15 11:07:02 +0800

    Initial Commit

---
+++ TRAIN-Calculator/ViewModels/PayrollViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TRAIN_Calculator.Models;
+
+namespace TRAIN_Calculator.ViewModels
+{
+    public class PayrollViewModel
+    {
+        public PaySlip PaySlips { get; set; }
+        public PaySlipCompensation PaySlipCompensations { get; set; }
+        public PaySlipDeduction PaySlipDeductions { get; set; }
+        public PaySlipDeMinimis PaySlipDeMinimis { get; set; }
+
+    }
+
+    
+}
